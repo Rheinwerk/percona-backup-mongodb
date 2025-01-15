4d5f7e159d8110d865a35b283b6d1e875ad94f23
Alexander Berresch <alexander.berresch@centerdevice.com>
2025-01-15 15:27:42 +0100

    Use io.Pipe and wait until all data are decrypted

--- pbm/gpg/decrypt.go
+++ pbm/gpg/decrypt.go
@@ -1,18 +1,36 @@
 package gpg
 
 import (
+	"github.com/pkg/errors"
 	"golang.org/x/crypto/openpgp"
 	"io"
-
-	"github.com/pkg/errors"
 )
 
 func Decrypt(data io.Reader, keyring openpgp.KeyRing) (io.Reader, error) {
-	message, err := openpgp.ReadMessage(data, keyring, nil, nil)
+	pr, pw := io.Pipe()
 
-	if err != nil {
-		return nil, errors.WithMessage(err, "gpg decrypt")
+	go func() {
+		defer pw.Close()
+		_, err := io.Copy(pw, data)
+		if err != nil {
+			pw.CloseWithError(err)
+		}
+	}()
+
+	done := make(chan struct{})
+	var message *openpgp.MessageDetails
+	var err error
+
+	go func() {
+		message, err = openpgp.ReadMessage(pr, keyring, nil, nil)
+		close(done)
+	}()
+
+	select {
+	case <-done:
+		if err != nil {
+			return nil, errors.New("gpg decrypt failed: " + err.Error())
+		}
+		return message.UnverifiedBody, nil
 	}
-
-	return message.UnverifiedBody, nil
 }
