0b531c0f0efe9f3f07f6eaf154c185237c7f414f
Alexander Berresch <alexander.berresch@centerdevice.com>
2025-01-20 15:13:53 +0100

    Revert "Use io.Pipe and wait until all data are decrypted"
    This reverts commit 52211e1ed4fe00b93c92de75d53887edf5dd9cca.

--- pbm/gpg/decrypt.go
+++ pbm/gpg/decrypt.go
@@ -1,36 +1,18 @@
 package gpg
 
 import (
-	"github.com/pkg/errors"
 	"golang.org/x/crypto/openpgp"
 	"io"
+
+	"github.com/pkg/errors"
 )
 
 func Decrypt(data io.Reader, keyring openpgp.KeyRing) (io.Reader, error) {
-	pr, pw := io.Pipe()
+	message, err := openpgp.ReadMessage(data, keyring, nil, nil)
 
-	go func() {
-		defer pw.Close()
-		_, err := io.Copy(pw, data)
-		if err != nil {
-			pw.CloseWithError(err)
-		}
-	}()
-
-	done := make(chan struct{})
-	var message *openpgp.MessageDetails
-	var err error
-
-	go func() {
-		message, err = openpgp.ReadMessage(pr, keyring, nil, nil)
-		close(done)
-	}()
-
-	select {
-	case <-done:
-		if err != nil {
-			return nil, errors.New("gpg decrypt failed: " + err.Error())
-		}
-		return message.UnverifiedBody, nil
+	if err != nil {
+		return nil, errors.WithMessage(err, "gpg decrypt")
 	}
+
+	return message.UnverifiedBody, nil
 }
